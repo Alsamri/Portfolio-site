b2299777977738a2c31f2d9a9970df2fa9c88feb
Alsamri <azozid@yahoo.com>
2025-03-13 18:08:18 +0000

    added types for assets

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,14 +2,14 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename: string = fileURLToPath(import.meta.url);
+const __dirname: string = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig: unknown[] = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
